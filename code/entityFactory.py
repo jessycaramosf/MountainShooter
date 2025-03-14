11ca222f1098f89a1588e76d2f54d7147f6c1421
jessycaramos <jessycaramosf@gmail.com>
2025-03-14 19:41:00 -0300

    Implement of backgroun on level 1

--- code/entityFactory.py
+++ code/entityFactory.py
@@ -6,11 +6,11 @@ from code.backgorund import Backgorund
 class EntityFactory:
 
     @staticmethod
-    def get_entity(entity_name: str, position=(0, 0)):
+    def get_entity(entity_name: str, position=(0,0)):
         match entity_name:
-    #fazer uma lista genérica com o laço for, auziliza a não ter que criar uma lista para cada um dos Backgrounds
+    #fazer uma lista genérica com o laço for, auxilia a não ter que criar uma lista para cada um dos Backgrounds
             case 'Level1Bg':
                 list_bg = []
                 for i in range(7):
-                    list_bg.apend(Backgorund(f'Level1Bg{i}', (0,0)))
+                    list_bg.append(Backgorund(f'Level1Bg{i}', (0,0)))
                 return list_bg
