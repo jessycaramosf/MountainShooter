4bce318909d4779e63293961a130fc135e378536
jessycaramos <jessycaramosf@gmail.com>
2025-03-14 18:10:54 -0300

    Updade Menu file Adding Key responses Update Events

--- code/menu.py
+++ code/menu.py
@@ -4,7 +4,7 @@ import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+from code.const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
 
 
 class Menu:
@@ -14,24 +14,45 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)  # metodo insere retangulo onde a imagem vai ser inserida
 
     def run(self, ):
+        menu_option = 0
         pygame.mixer_music.load('./asset/Menu.mp3')
         pygame.mixer_music.play(-1)
         while True:
+            # DRAW IMAGES
             self.window.blit(source=self.surf,
                              dest=self.rect)  # metodo que atualiza a imagem dentro do objeto da surface
             # onde source é a fonte do desenho e o dest é onde o desenho da source vai ser colocado
             self.menu_text(text_size=50, text="Mountain", text_color= COLOR_ORANGE, text_center_pos=((WIN_WIDTH / 2), 70))
             self.menu_text(text_size=50, text="Shooter", text_color= COLOR_ORANGE, text_center_pos=((WIN_WIDTH / 2), 120))
+            # Change colors in menu
             for i in range(len(MENU_OPTION)):
-                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
 
-            pygame.display.flip()  # atualiza a tela para a imagem aparecer na tela
 
             # Check for all events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()  # Close Window
                     quit()  # End Game
+                if event.type == pygame.KEYDOWN: # Response when the Key is pressed
+                    if event.key == pygame.K_DOWN: #DownKey
+                        if menu_option <len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP: #UpKey
+                        if menu_option > 0:
+                            menu_option -=1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN: # Enter
+                        return MENU_OPTION[menu_option]
+
+            pygame.display.flip()  # Update the screen (IMAGE ON SCREEN)
+
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
