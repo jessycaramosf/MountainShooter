0b71fb7373cedf2cb914f37af7476a81c512cc93
jessycaramos <jessycaramosf@gmail.com>
2025-03-14 19:13:15 -0300

    Updating entity and entityFactory.py First steps of BG paralax

--- code/entityFactory.py
+++ code/entityFactory.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from code.backgorund import Backgorund
+
 
 class EntityFactory:
-    def __init__(self):
-        pass
 
-    def get_entity(self, entity_type):
-        pass
+    @staticmethod
+    def get_entity(entity_name: str, position=(0, 0)):
+        match entity_name:
+    #fazer uma lista genérica com o laço for, auziliza a não ter que criar uma lista para cada um dos Backgrounds
+            case 'Level1Bg':
+                list_bg = []
+                for i in range(7):
+                    list_bg.apend(Backgorund(f'Level1Bg{i}', (0,0)))
+                return list_bg
